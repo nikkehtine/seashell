a4a67afd9224fae8d19aa98e75ca3a980287b32b
nikkehtine <nikkehtine@int.pl>
2024-03-10 18:18:03 +0100

    Add exit command (it doesn't work)

--- src/repl.c
+++ src/repl.c
@@ -1,5 +1,7 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "repl.h"
@@ -23,13 +25,51 @@ int repl(void)
         printf("%s", PROMPT);
         char buf[MAX];
         fgets(buf, MAX, stdin);
-        if (strlen(buf) == 1 && buf[0] == 10)
+
+        if (strlen(buf) == 1 && isspace(buf[0]))
         {
             continue;
         }
-        else
+
+        // Naive parsing
+        char *command = strtok(buf, " ");
+        if (command != NULL && strcmp(buf, "exit") == 0)
         {
-            repl_halt(&running, &exit_code, 0);
+            int arg_exit_code;
+            char *arg0 = strtok(buf, " ");
+            if (arg0 == NULL)
+            {
+                arg_exit_code = 0;
+            }
+            else
+            {
+                bool arg_is_not_num = false;
+                for (size_t i = 0; i < strlen(arg0); i++)
+                {
+                    if (!isdigit(arg0[i]))
+                    {
+                        arg_is_not_num = true;
+                        break;
+                    }
+                }
+
+                int arg0_int;
+
+                if (arg_is_not_num)
+                {
+                    arg0_int = atoi(arg0);
+                }
+                else
+                {
+                    arg0_int = 1;
+                }
+                arg_exit_code = arg0_int;
+            }
+            repl_halt(&running, &exit_code, arg_exit_code);
+        }
+        else if (command != NULL)
+        {
+            printf(">>> %s\n", command);
         }
     }
 
