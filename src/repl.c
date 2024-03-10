a61c70ce2ea05af73ec9587070482084eb45faa5
nikkehtine <nikkehtine@int.pl>
2024-03-10 16:55:26 +0100

    Accept user input within STDIN

--- src/repl.c
+++ src/repl.c
@@ -1,11 +1,13 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "repl.h"
 
-const char PROMPT[] = "$ ";
+const char PROMPT[3] = "$ ";
+const int MAX = 256;
 
-void repl_halt(bool *running, int *exit_code)
+void repl_halt(bool *running, int *exit_code, int status_code)
 {
     *running = false;
     *exit_code = 0;
@@ -18,8 +20,17 @@ int repl(void)
 
     while (running)
     {
-        printf("%s\n", PROMPT);
-        repl_halt(&running, &exit_code);
+        printf("%s", PROMPT);
+        char buf[MAX];
+        fgets(buf, MAX, stdin);
+        if (strlen(buf) == 1 && buf[0] == 10)
+        {
+            continue;
+        }
+        else
+        {
+            repl_halt(&running, &exit_code, 0);
+        }
     }
 
     return exit_code;
